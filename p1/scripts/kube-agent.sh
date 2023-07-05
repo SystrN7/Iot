43413dfa3818a911d014fb502d851d9155b9e633
SystrN7 <felixgalaup@yahoo.fr>
2023-07-05 14:25:51 +0200

    wip

--- p1/scripts/kube-agent.sh
+++ p1/scripts/kube-agent.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://192.168.56.110:6443 --token $K3S_CLUSTER_TOKEN" sh -s -
-
 IP_ADDRESS=$(ip a show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f1)
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --node-ip=${IP_ADDRESS} --flannel-iface=enp0s8" K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/tmp/node-token) sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /vagrant/tmp/node-token) sh -
 
 
 # Copy kube config to user home (for kuectl)
--- p1/scripts/kube-server.sh
+++ p1/scripts/kube-server.sh
@@ -5,7 +5,7 @@ IP_ADDRESS=$(ip a show enp0s8 | grep "inet " | awk '{print $2}' | cut -d / -f1)
 NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
 KUBE_CONFIG="/etc/rancher/k3s/k3s.yaml"
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=${IP_ADDRESS} --flannel-iface=enp0s8 --write-kubeconfig-mode 644" sh -
+curl -sfL https://get.k3s.io | sh -
 
 while [ ! -e ${NODE_TOKEN} ]
 do
@@ -17,10 +17,7 @@ cat ${NODE_TOKEN}
 mkdir -p /vagrant/tmp
 cp ${NODE_TOKEN} /vagrant/tmp
 cp ${KUBE_CONFIG} /vagrant/tmp #copy contents of "k3s.yaml" to ".kube/config" to 'kubectl' from local-machine
-
-
-#curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --token $K3S_CLUSTER_TOKEN
-
+echo ${IP_ADDRESS} > /vagrant/tmp/server-address
 
 
 # Copy kube config to user home (To use kuectl)
