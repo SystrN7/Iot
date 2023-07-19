ab7e2e5ef50b09233040952afa0b5e9b8bb6928a
SystrN7 <felixgalaup@yahoo.fr>
2023-07-19 10:31:34 +0200

    fix docker install/remove

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -4,10 +4,15 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# Install docker
-curl -sfL https://get.docker.com | sh
-groupadd docker
-sudo usermod -aG docker $USER
+# test if docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+    # Install docker
+    curl -sfL https://get.docker.com | sh
+    groupadd docker
+    sudo usermod -aG docker $USER
+
+    sleep 10 # wait for docker to start
+fi
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -15,17 +20,16 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
-# Install k3d
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.5.1 bash
-# Create cluster
-sudo k3d cluster create argocd --api-port 6443 -p 8080:80@loadbalancer --agents 2
-
-mkdir -p ~/.kube
-sudo k3d kubeconfig get argocd > ~/.kube/config
-
 # Argocd CLI
 curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
+# Install k3d
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.5.1 bash
+# Create cluster
+sudo k3d cluster create argocd --api-port 6443 -p 8080:80@loadbalancer --agents 2
 
+# Configure kubectl
+mkdir -p ~/.kube
+sudo k3d kubeconfig get argocd > ~/.kube/config
--- p3/scripts/uninstall.sh
+++ p3/scripts/uninstall.sh
@@ -4,7 +4,12 @@
 k3d cluster delete -a
 sudo rm $(which k3d)
 
-# Remove kubectl and podman
-sudo apt-get remove podman -y
+# Remove docker
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+# Remove all images, containers, volumes, and networks (doesn't remove durring purge)
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+
+# Remove kubectl and argocd CLI
 sudo rm /usr/local/bin/kubectl
 sudo rm /usr/local/bin/argocd
