032dbf4f554cb9cda9c008bf461dbdadc920997a
SystrN7 <felixgalaup@yahoo.fr>
2023-07-20 10:20:50 +0200

    Fix argocd install and app deployments.

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -3,8 +3,8 @@ kubectl create namespace argocd
 kubectl create namespace dev
 
 # Install ArgoCD
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -n argocd -f confs/argocd-cmd-params-cm.yaml
+kubectl apply -n argocd -f confs/argocd.yaml
 kubectl apply -n argocd -f confs/argocd-ingress.yaml
 
 # Install 
@@ -13,14 +13,23 @@ argocd login --core
 # https://stackoverflow.com/questions/69483408/argocd-getting-started-guide-getting-fata0000-configmap-argocd-cm-not-found
 kubectl config set-context --current --namespace=argocd
 
+while [[ $(kubectl get pods -l  app.kubernetes.io/name=argocd-server -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]];
+do
+    echo "Waiting argocd start ..."
+    sleep 10
+done
+
+echo "Wait for argocd to start extra delai (180s)"
+sleep 180 # wait for argocd to start
+
 argocd app create fgalaup-iot \
  --server localhost \
  --dest-namespace dev \
  --dest-server https://kubernetes.default.svc \
- --repo https://github.com/argoproj/argocd-example-apps.git \
- --path /
+ --repo https://github.com/SystrN7/fgalaup_iot.git \
+ --path Apps
 
- argocd app sync fgalaup-iot
+argocd app set fgalaup-iot --sync-policy automated
 
 # Get argocd Password
 echo "Argocd login : admin"
