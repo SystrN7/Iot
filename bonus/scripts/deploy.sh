31dc54d9fe757cbffb02d59c6826478095adadc2
SystrN7 <felixgalaup@yahoo.fr>
2023-07-21 10:32:28 +0200

    Add begin of bonus parts

---
+++ bonus/scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Add Gitlab to hosts file
+if [ -f /etc/hosts.bak ]; then
+    sudo bash -c 'cat confs/hosts >> /etc/hosts'
+fi
+
+# Create kube namesapce
+kubectl create namespace gitlab
+
+# Install Gitlab
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
+    --timeout 600s \
+    --set global.edition=ce \
+    --set global.hosts.domain=gitlab.cluster \
+    --set global.hosts.externalIP=127.0.0.1 \
+    --set certmanager.install=false \
+    --set global.ingress.configureCertmanager=false \
+    --set gitlab-runner.install=false
---
+++ bonus/scripts/reset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Clean up deployment
+kubectl delete namespace gitlab
